672541acc2e39b2e8f4e4bd9109a266b3781e5c6
oscar2697 <lindooscar635@gmail.com>
2025-02-28 21:22:18 -0500

    Add files to repository

---
+++ next.config.ts
@@ -0,0 +1,31 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100MB'
+    }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com'
+      }, 
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com'
+      }, 
+      {
+        protocol: 'https',
+        hostname: 'cdn.appwrite.io'
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+    ]
+  }
+};
+
+export default nextConfig;
